35d0ed8c4a19ad98d41ee9f8b968bf90bc0f4435
Zinklof <123103261+Zinklof@users.noreply.github.com>
2024-10-23 04:52:22 -0400

    MathZ added
    Yippie!

---
+++ Utils/MathZ/Numbers.cs
@@ -0,0 +1,139 @@
+﻿
+
+using System;
+
+namespace ZinklofDev.Utils.MathZ
+{
+    /// <summary>
+    /// Doing math with El Numeros
+    /// </summary>
+    static public class Numbers
+    {
+        /// <summary>
+        /// Provides the Square Value of a number.
+        /// </summary>
+        /// <param name="x">Value to square</param>
+        /// <returns>Sqaured Value of X</returns>
+        static public double Sqr(double x) { return x * x; }
+        /// <summary>
+        /// Provides the Square Value of a number.
+        /// </summary>
+        /// <param name="x">Value to square</param>
+        /// <returns>Sqaured Value of X</returns>
+        static public float Sqr(float x) { return x * x; }
+        /// <summary>
+        /// Provides the Square Value of a number.
+        /// </summary>
+        /// <param name="x">Value to square</param>
+        /// <returns>Sqaured Value of X</returns>
+        static public int Sqr(int x) { return x * x; }
+        /// <summary>
+        /// Provides the Square Value of a number.
+        /// </summary>
+        /// <param name="x">Value to square</param>
+        /// <returns>Sqaured Value of X</returns>
+        static public long Sqr(long x) { return x * x; }
+        /// <summary>
+        /// Provides the Square Value of a number.
+        /// </summary>
+        /// <param name="x">Value to square</param>
+        /// <returns>Sqaured Value of X</returns>
+        static public short Sqr(short x) { return (short)(x * x); }
+        /// <summary>
+        /// Provides the Square Value of a number.
+        /// </summary>
+        /// <param name="x">Value to square</param>
+        /// <returns>Sqaured Value of X</returns>
+        static public uint Sqr(uint x) { return x * x; }
+        /// <summary>
+        /// Provides the Square Value of a number.
+        /// </summary>
+        /// <param name="x">Value to square</param>
+        /// <returns>Sqaured Value of X</returns>
+        static public ulong Sqr(ulong x) { return x * x; }
+        /// <summary>
+        /// Provides the Square Value of a number.
+        /// </summary>
+        /// <param name="x">Value to square</param>
+        /// <returns>Sqaured Value of X</returns>
+        static public ushort Sqr(ushort x) { return (ushort)(x * x); }
+        /// <summary>
+        /// Provides the Cubed Value of a number.
+        /// </summary>
+        /// <param name="x">Value to cube</param>
+        /// <returns>Cubed Value of X</returns>
+        static public double Cube(double x) { return x * x * x; }
+        /// <summary>
+        /// Provides the Cubed Value of a number.
+        /// </summary>
+        /// <param name="x">Value to cube</param>
+        /// <returns>Cubed Value of X</returns>
+        static public float Cube(float x) { return x * x * x; }
+        /// <summary>
+        /// Provides the Cubed Value of a number.
+        /// </summary>
+        /// <param name="x">Value to cube</param>
+        /// <returns>Cubed Value of X</returns>
+        static public int Cube(int x) { return x * x * x; }
+        /// <summary>
+        /// Provides the Cubed Value of a number.
+        /// </summary>
+        /// <param name="x">Value to cube</param>
+        /// <returns>Cubed Value of X</returns>
+        static public long Cube(long x) { return x * x * x; }
+        /// <summary>
+        /// Provides the Cubed Value of a number.
+        /// </summary>
+        /// <param name="x">Value to cube</param>
+        /// <returns>Cubed Value of X</returns>
+        static public short Cube(short x) { return (short)(x * x * x); }
+        /// <summary>
+        /// Provides the Cubed Value of a number.
+        /// </summary>
+        /// <param name="x">Value to cube</param>
+        /// <returns>Cubed Value of X</returns>
+        static public uint Cube(uint x) { return x * x * x; }
+        /// <summary>
+        /// Provides the Cubed Value of a number.
+        /// </summary>
+        /// <param name="x">Value to cube</param>
+        /// <returns>Cubed Value of X</returns>
+        static public ulong Cube(ulong x) { return x * x * x; }
+        /// <summary>
+        /// Provides the Cubed Value of a number.
+        /// </summary>
+        /// <param name="x">Value to cube</param>
+        /// <returns>Cubed Value of X</returns>
+        static public ushort Cube(ushort x) { return (ushort)(x * x * x); }
+        /// <summary>
+        /// Returns X to the Y power.
+        /// </summary>
+        /// <param name="x">Number to raise to Y power</param>
+        /// <param name="y">power to raise X to</param>
+        /// <returns>X^Y / X to the Y power</returns>
+        static public double Power(double x, double y)
+        {
+            double temp = x;
+            for (int i = 0; i <= y; i++)
+            {
+                temp = temp * x;
+            }
+            return temp;
+        }
+        /// <summary>
+        /// Returns X to the Y power.
+        /// </summary>
+        /// <param name="x">Number to raise to Y power</param>
+        /// <param name="y">power to raise X to</param>
+        /// <returns>X^Y / X to the Y power</returns>
+        static public float Power(float x, float y)
+        {
+            float temp = x;
+            for (int i = 0; i <= y; i++)
+            {
+                temp = temp * x;
+            }
+            return temp;
+        }
+    }
+}
---
+++ Utils/MathZ/Vectors.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using UnityEngine;
+
+namespace ZinklofDev.Utils.MathZ
+{
+    /// <summary>
+    /// Vectors? VECTOR MATH BABY!
+    /// </summary>
+    static public class Vectors
+    {
+        /// <summary>
+        /// Provides the Sqaure Distance between two points in one Dimension. (cheaper than sqrt distance)
+        /// </summary>
+        /// <param name="x">Point 1</param>
+        /// <param name="y">Point 2</param>
+        /// <returns>Square Distance between Point 1 and Point 2</returns>
+        static public double SqrDist1(double x, double y)
+        {
+            return Math.Abs(x - y);
+        }
+        /// <summary>
+        /// Provides the Sqaure Distance between two points in two Dimensions. (cheaper than sqrt distance)
+        /// </summary>
+        /// <param name="x">Point 1</param>
+        /// <param name="y">Point 2</param>
+        /// <returns>Square Distance between Point 1 and Point 2</returns>
+        static public double SqrDist2(Vector2 x, Vector2 y)
+        {
+            return Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * y.y));
+        }
+        /// <summary>
+        /// Provides the Sqaure Distance between two points in three Dimensions. (cheaper than sqrt distance)
+        /// </summary>
+        /// <param name="x">Point 1</param>
+        /// <param name="y">Point 2</param>
+        /// <returns>Square Distance between Point 1 and Point 2</returns>
+        static public double SqrDist3(Vector3 x, Vector3 y)
+        {
+            return (Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * x.y)) + Numbers.Sqr((x.z * y.z)));
+        }
+        /// <summary>
+        /// Provides the Sqaure Distance between two points in four Dimensions. (cheaper than sqrt distance)
+        /// </summary>
+        /// <param name="x">Point 1</param>
+        /// <param name="y">Point 2</param>
+        /// <returns>Square Distance between Point 1 and Point 2</returns>
+        static public double SqrDist4(Vector4 x, Vector4 y)
+        {
+            return (Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * x.y)) + Numbers.Sqr((x.z * y.z)) + Numbers.Sqr((x.w * y.w)));
+        }
+        /// <summary>
+        /// Provides the Sqaure Distance between two points in one Dimension. (cheaper than sqrt distance)
+        /// </summary>
+        /// <param name="x">Point 1</param>
+        /// <param name="y">Point 2</param>
+        /// <returns>Square Distance between Point 1 and Point 2</returns>
+        static public float SqrDist1f(float x, float y)
+        {
+            return Math.Abs(x - y);
+        }
+        /// <summary>
+        /// Provides the Sqaure Distance between two points in two Dimensions. (cheaper than sqrt distance)
+        /// </summary>
+        /// <param name="x">Point 1</param>
+        /// <param name="y">Point 2</param>
+        /// <returns>Square Distance between Point 1 and Point 2</returns>
+        static public float SqrDist2f(Vector2 x, Vector2 y)
+        {
+            return Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * y.y));
+        }
+        /// <summary>
+        /// Provides the Sqaure Distance between two points in three Dimensions. (cheaper than sqrt distance)
+        /// </summary>
+        /// <param name="x">Point 1</param>
+        /// <param name="y">Point 2</param>
+        /// <returns>Square Distance between Point 1 and Point 2</returns>
+        static public float SqrDist3f(Vector3 x, Vector3 y)
+        {
+            return (Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * x.y)) + Numbers.Sqr((x.z * y.z)));
+        }
+        /// <summary>
+        /// Provides the Sqaure Distance between two points in four Dimensions. (cheaper than sqrt distance)
+        /// </summary>
+        /// <param name="x">Point 1</param>
+        /// <param name="y">Point 2</param>
+        /// <returns>Square Distance between Point 1 and Point 2</returns>
+        static public float SqrDist4f(Vector4 x, Vector4 y)
+        {
+            return (Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * x.y)) + Numbers.Sqr((x.z * y.z)) + Numbers.Sqr((x.w * y.w)));
+        }
+    }
+}
