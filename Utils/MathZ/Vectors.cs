13b747bd097760f567f65c81a7d26f7e20c5beed
Zinklof <123103261+Zinklof@users.noreply.github.com>
2024-10-25 05:34:40 -0400

    the greatest programmer to ever live
    Fixed the vecotr math multiplying instead of subtracting values. also fixed the SqrDist3 and SqrDist4 (plus their float versions) doing x.y - x.y instead of x.y - y.y.

--- Utils/MathZ/Vectors.cs
+++ Utils/MathZ/Vectors.cs
@@ -26,7 +26,7 @@ namespace ZinklofDev.Utils.MathZ
         /// <returns>Square Distance between Point 1 and Point 2</returns>
         static public double SqrDist2(Vector2 x, Vector2 y)
         {
-            return Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * y.y));
+            return Numbers.Sqr((x.x - y.x)) + Numbers.Sqr((x.y - y.y));
         }
         /// <summary>
         /// Provides the Sqaure Distance between two points in three Dimensions. (cheaper than sqrt distance)
@@ -36,7 +36,7 @@ namespace ZinklofDev.Utils.MathZ
         /// <returns>Square Distance between Point 1 and Point 2</returns>
         static public double SqrDist3(Vector3 x, Vector3 y)
         {
-            return (Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * x.y)) + Numbers.Sqr((x.z * y.z)));
+            return (Numbers.Sqr((x.x - y.x)) + Numbers.Sqr((x.y - y.y)) + Numbers.Sqr((x.z - y.z)));
         }
         /// <summary>
         /// Provides the Sqaure Distance between two points in four Dimensions. (cheaper than sqrt distance)
@@ -46,7 +46,7 @@ namespace ZinklofDev.Utils.MathZ
         /// <returns>Square Distance between Point 1 and Point 2</returns>
         static public double SqrDist4(Vector4 x, Vector4 y)
         {
-            return (Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * x.y)) + Numbers.Sqr((x.z * y.z)) + Numbers.Sqr((x.w * y.w)));
+            return (Numbers.Sqr((x.x - y.x)) + Numbers.Sqr((x.y - y.y)) + Numbers.Sqr((x.z - y.z)) + Numbers.Sqr((x.w - y.w)));
         }
         /// <summary>
         /// Provides the Sqaure Distance between two points in one Dimension. (cheaper than sqrt distance)
@@ -66,7 +66,7 @@ namespace ZinklofDev.Utils.MathZ
         /// <returns>Square Distance between Point 1 and Point 2</returns>
         static public float SqrDist2f(Vector2 x, Vector2 y)
         {
-            return Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * y.y));
+            return Numbers.Sqr((x.x - y.x)) + Numbers.Sqr((x.y - y.y));
         }
         /// <summary>
         /// Provides the Sqaure Distance between two points in three Dimensions. (cheaper than sqrt distance)
@@ -76,7 +76,7 @@ namespace ZinklofDev.Utils.MathZ
         /// <returns>Square Distance between Point 1 and Point 2</returns>
         static public float SqrDist3f(Vector3 x, Vector3 y)
         {
-            return (Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * x.y)) + Numbers.Sqr((x.z * y.z)));
+            return (Numbers.Sqr((x.x - y.x)) + Numbers.Sqr((x.y - y.y)) + Numbers.Sqr((x.z - y.z)));
         }
         /// <summary>
         /// Provides the Sqaure Distance between two points in four Dimensions. (cheaper than sqrt distance)
@@ -86,7 +86,7 @@ namespace ZinklofDev.Utils.MathZ
         /// <returns>Square Distance between Point 1 and Point 2</returns>
         static public float SqrDist4f(Vector4 x, Vector4 y)
         {
-            return (Numbers.Sqr((x.x * y.x)) + Numbers.Sqr((x.y * x.y)) + Numbers.Sqr((x.z * y.z)) + Numbers.Sqr((x.w * y.w)));
+            return (Numbers.Sqr((x.x - y.x)) + Numbers.Sqr((x.y - y.y)) + Numbers.Sqr((x.z - y.z)) + Numbers.Sqr((x.w - y.w)));
         }
     }
 }
