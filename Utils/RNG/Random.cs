5748b3ac00a4a4db54485fcc89d9980eed3400c2
Zink <zinklofbusiness@gmail.com>
2025-03-06 13:13:38 -0500

    Update Random.cs
    some PCG-XSL-RR implementation.

--- Utils/RNG/Random.cs
+++ Utils/RNG/Random.cs
@@ -1,11 +1,53 @@
 using System;
 
+/*
+techreport | oneill:pcg2014
+
+title : PCG: A Family of Simple Fast Space-Efficient Statistically Good Algorithms for Random Number Generation
+author : Melissa E. O'Neill
+institution : Harvey Mudd College
+address : Claremont, CA
+number : HMC-CS-2014-0905
+year : 2014
+month : Sep
+xurl : https://www.cs.hmc.edu/tr/hmc-cs-2014-0905.pdf
+*/
+
 /// <summary>
-/// Zrand is a random number generation class that uses the PCG RNG algorithm, which is code line efficient, memory efficient, speed efficient, and pretty random compared to other PRNGs. (Dont ask my source, I trusted their site :D) | 
-/// This particular implimentation uses a 64 bit state, 64 bit increment, and 64 bit multiplier, resulting in 24 bytes of memory usage, compared to the base C# random class using 232 (that number on the base random comes from a random github user, don't quote me) |
-/// Specifically this is the PCG-XSH-RR
+/// Zrand is a random number generation class that uses the PCG RNG family of algorithms, which are code line efficient, memory efficient, speed efficient, and pretty random compared to other PRNGs. (Dont ask my source, I trusted their site :D) | 
+/// Specifically this is the PCG-XSL-RR 128/64 algorithm. which has a state of 128 bits, and can output 64 bit numbers, and is one of the fastest members in the family, albeit not as good statistically speaking with its outputs. |
+/// Uses 16 bytes of memory when existing, and an additional 
 /// </summary>
-public class Zrand // this doesn't break naming convention, it is meant to be one whole word :D
+public class Zrand64
 {
-    // to impliment at home. will also make a version using PCG-XSH-RS which is slightly faster and uses 8 less bytes of memory, at the cost of having less possible outputs.
+    private ulong State;
+    private ulong inc;
+
+    ulong Next()
+    {
+        ulong oldState = state;
+
+        state = oldState * 6364136223846793005U + inc;
+
+        ulong XorShift = ((oldState >> 18) ^ oldState) >> 27;
+        ulong rot = oldState >> 59;
+
+        return (XorShift >> rot) ^ (XorShift << (64 - rot));
+    }
+
+    ulong NextRange(ulong min, ulong max)
+    {
+        ulong range_width = max - min + 1;
+        return min + (Next() % range_width);
+    }
+
+    double NextDouble() // 0-1
+    {
+        return (Double)(Next() / ulong.MaxValue());
+    }
+
+    Double DoubleRange(Double min, Double max);
+    {
+        return (Double)((min + (min - max)) * (Next() / ulong.MaxValue()));
+    }
 }
